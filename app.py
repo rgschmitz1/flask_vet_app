cc75278223d0b3764f28a69d2d0620a7c99c49d9
Danielle <dlambion@gmail.com>
2022-05-17 22:43:04 -0700

    added caption to appt and aggregate page

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ import forms
 from blueprint.home import home
 from blueprint.patient import patient
 from blueprint.appointment import appointment
+from blueprint.aggregate import aggregate
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
@@ -19,6 +20,7 @@ app.config.from_object('config.Config')
 app.register_blueprint(home)
 app.register_blueprint(patient)
 app.register_blueprint(appointment)
+app.register_blueprint(aggregate)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
