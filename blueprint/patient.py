6d8374c9f2b3ca56e95266e4ec7eb935da70141e
Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>
2022-05-22 23:26:48 -0700

    Added patient_id template
    Signed-off-by: Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>

--- blueprint/patient.py
+++ blueprint/patient.py
@@ -11,7 +11,8 @@ from postgres import postgres
 patient = Blueprint('patient', __name__)
 
 @patient.route('/patient', methods=["GET", "POST"])
-def patient_info():
+@patient.route('/patient/<pet_id>', methods=["GET", "POST"])
+def patient_info(pet_id=None):
     form = InsertPatient()
     pg = postgres()
     if request.method != "GET":
@@ -28,9 +29,22 @@ def patient_info():
                 flash('ERROR: Failed to add patient to database.')
         else:
             flash('ERROR: Issue in patient info, please check inputs')
-    animals = pg.execute_read_query("SELECT * FROM veterinarian_office.animal ORDER BY pet_id")
-    return render_template(
-        'patient.html',
-        form=form,
-        animals=animals
-    )
+    if pet_id:
+        allergies = pg.execute_read_query(f"SELECT allergy FROM veterinarian_office.allergy WHERE pet_id = '{pet_id}'")
+        conditions = pg.execute_read_query(f"SELECT medical_condition, is_active FROM veterinarian_office.condition WHERE pet_id = '{pet_id}'")
+        prescriptions = pg.execute_read_query(f"SELECT prescription, appointment_date FROM veterinarian_office.prescription WHERE pet_id = '{pet_id}'")
+        appointments = pg.execute_read_query(f"SELECT appointment_date, vet_id, weight, notes FROM veterinarian_office.appointment WHERE pet_id = '{pet_id}'")
+        return render_template(
+            'patient_id.html',
+            allergies=allergies,
+            appointments=appointments,
+            conditions=conditions,
+            prescriptions=prescriptions,
+        )
+    else:
+        animals = pg.execute_read_query("SELECT * FROM veterinarian_office.animal ORDER BY pet_id")
+        return render_template(
+            'patient.html',
+            form=form,
+            animals=animals
+        )
