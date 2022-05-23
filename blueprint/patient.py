891781048bc24dc438759eafd350d03d977336d8
Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>
2022-05-23 13:00:30 -0700

    Updated patient_id template to display vet name instead of vet id
    Signed-off-by: Bob Schmitz III <14095796+rgschmitz1@users.noreply.github.com>

--- blueprint/patient.py
+++ blueprint/patient.py
@@ -13,9 +13,14 @@ patient = Blueprint('patient', __name__)
 @patient.route('/patient', methods=["GET", "POST"])
 @patient.route('/patient/<pet_id>', methods=["GET", "POST"])
 def patient_info(pet_id=None):
+    """ This function adds new patients along with displaying all patients records
+
+    input id: optional numeric patient id to display detailed paitient records
+    """
     form = InsertPatient()
     pg = postgres()
-    if request.method != "GET":
+    # If a POST request is made and pet_id is None, then add a new paitient
+    if request.method == "POST" and pet_id == None:
         if form.validate_on_submit():
             query = f"""INSERT INTO veterinarian_office.animal(
                     pet_name, pet_birthdate, pet_sex, species, breed, color
@@ -29,11 +34,16 @@ def patient_info(pet_id=None):
                 flash('ERROR: Failed to add patient to database.')
         else:
             flash('ERROR: Issue in patient info, please check inputs')
+    # Detailed paitient records view
     if pet_id:
         allergies = pg.execute_read_query(f"SELECT allergy FROM veterinarian_office.allergy WHERE pet_id = '{pet_id}'")
         conditions = pg.execute_read_query(f"SELECT medical_condition, is_active FROM veterinarian_office.condition WHERE pet_id = '{pet_id}'")
-        prescriptions = pg.execute_read_query(f"SELECT prescription, appointment_date FROM veterinarian_office.prescription WHERE pet_id = '{pet_id}'")
-        appointments = pg.execute_read_query(f"SELECT appointment_date, vet_id, weight, notes FROM veterinarian_office.appointment WHERE pet_id = '{pet_id}'")
+        prescriptions = pg.execute_read_query(f"""SELECT p.prescription AS prescription, p.appointment_date appointment_date, v.vet_name AS vet_name
+            FROM veterinarian_office.prescription p, veterinarian_office.vet v, veterinarian_office.appointment a
+            WHERE p.pet_id = '{pet_id}' AND p.appointment_date = a.appointment_date AND a.vet_id = v.vet_id""")
+        appointments = pg.execute_read_query(f"""SELECT a.appointment_date AS appointment_date, v.vet_name AS vet_name, a.weight AS weight, a.notes AS notes
+            FROM veterinarian_office.appointment a, veterinarian_office.vet v
+            WHERE a.pet_id = '{pet_id}' AND a.vet_id = v.vet_id""")
         return render_template(
             'patient_id.html',
             allergies=allergies,
@@ -41,6 +51,7 @@ def patient_info(pet_id=None):
             conditions=conditions,
             prescriptions=prescriptions,
         )
+    # All paitient records
     else:
         animals = pg.execute_read_query("SELECT * FROM veterinarian_office.animal ORDER BY pet_id")
         return render_template(
