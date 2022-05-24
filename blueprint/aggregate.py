b34ff73f4959b83d1766d81e37bfe6591593a1c7
Rick Morrow <rclark.morrow@gmail.com>
2022-05-23 19:09:31 -0700

    cleanup code and add documentation

--- blueprint/aggregate.py
+++ blueprint/aggregate.py
@@ -20,6 +20,19 @@ aggregate = Blueprint('aggregate', __name__)
 
 @aggregate.route('/aggregate', methods=["GET", "POST"])
 def aggregate_info():
+    """
+        This route queries the database for aggregate data on patient
+        medical conditions and allergies for a specific species of patient.
+        It returns data with the column headings 'condition', 'type' and
+        'case_count'. Condition is an allergy or medical condition, type
+        specifies whether it is a medical condition or an allergy and
+        case_count is the total number of patients for a species affected
+        by that condition.
+
+        The endpoint uses the POST method to retrieve form data for the
+        species selected by the user and the GET method to return queried
+        data.
+    """
     pg = postgres()
     species = pg.execute_read_query("SELECT DISTINCT species FROM veterinarian_office.animal")
     species = [str(s).replace("['", "").replace("']", "") for s in species]
@@ -27,20 +40,11 @@ def aggregate_info():
     if curr_species is None:
         curr_species = str(species[0])
 
-    # medical_conditions = pg.execute_read_query("SELECT DISTINCT medical_condition, 'Medical Condition' as type, COUNT (medical_condition) FROM veterinarian_office.condition "+
-    #                                      "INNER JOIN veterinarian_office.animal "+
-    #                                      "ON veterinarian_office.condition.pet_id = veterinarian_office.animal.pet_id "+
-    #                                      "WHERE species='"+curr_species+"'" +
-    #                                      "GROUP BY medical_condition " +
-    #                                      "ORDER BY COUNT DESC")
-    #
-    # allergies = pg.execute_read_query("SELECT DISTINCT allergy, count (allergy) FROM veterinarian_office.allergy "+
-    #                                      "INNER JOIN veterinarian_office.animal "+
-    #                                      "ON veterinarian_office.allergy.pet_id = veterinarian_office.animal.pet_id "+
-    #                                      "WHERE species='"+curr_species+"'"
-    #                                      "GROUP BY allergy " +
-    #                                      "ORDER BY COUNT DESC")
-
+    # The conditions variable contains the results of a union of two queries.
+    # Each query returns the name and count of unique allergies and
+    # medical conditions affecting a species. It renames the allery and
+    # medical condition headers to be 'condition' and adds a new column
+    # 'type' that indicates whether the condition is medical or an allergy.
     conditions = pg.execute_read_query("SELECT DISTINCT medical_condition as condition, 'medical' as type, COUNT (medical_condition) as case_count FROM veterinarian_office.condition "+
                                          "INNER JOIN veterinarian_office.animal "+
                                          "ON veterinarian_office.condition.pet_id = veterinarian_office.animal.pet_id "+
@@ -54,25 +58,9 @@ def aggregate_info():
                                             "WHERE species='" + curr_species + "'"
                                             "GROUP BY allergy " +
                                             "ORDER BY case_count DESC")
-    print("CONDITIONS", conditions)
-    # print("ALLERGIES", allergies)
 
-    # medical_info = pg.execute_read_query("WITH PetAllergies AS("+
-    #                                "SELECT allergy, A.pet_id FROM
-    #                                veterinarian_office.allergy,
-    #                                veterinarian_office.animal A "+
-    #                                "WHERE A.pet_id =
-    #                                veterinarian_office.allergy.pet_id "+
-    #                                "AND A.species='"+curr_species+"') "+
-    #                                "SELECT DISTINCT * FROM PetAllergies "+
-    #                                "INNER JOIN veterinarian_office.condition C "+
-    #                                "ON C.pet_id = PetAllergies.pet_id ")
-
-
-
-    # allergies = [str(s).replace("['", "").replace("']", "") for s in allergies]
-    # conditions = [str(s).replace("['", "").replace("']", "") for s in conditions]
-
-
+    # Returns data to the template. It stores a default species, a
+    # selected species (curr_species) used in the queries and the conditions
+    # table that is returned to the view.
     return render_template('aggregate.html', species=species,
                            curr_species=curr_species, conditions=conditions)
